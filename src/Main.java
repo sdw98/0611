13d007300121122c8bfdc37155ce4578e925b492
sdw98 <sdw9851@gmail.com>
2025-06-11 17:49:26 +0900

    계산기 수정버전

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         double number2;
         double result = 0;
         boolean ok = true;
+        boolean keep = true;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -14,43 +15,54 @@ public class Main {
         System.out.print("첫번째 수를 입력해 주세요: ");
         number1 = scanner.nextDouble();
 
-        System.out.print("사칙연산(+,-,*,/) 중 골라주세요. ");
+        while(keep){
+            System.out.print("사칙연산(+,-,*,/) 중 골라주세요. ");
 
-        char operator = scanner.next().charAt(0);
-        if (operator == '/') {
-            while (true) {
-                System.out.print("두번째 수를 입력해 주세요 (0을 제외한 숫자): ");
-                number2 = scanner.nextDouble();
+            char operator = scanner.next().charAt(0);
+            if (operator == '/') {
+                while (true) {
+                    System.out.print("두번째 수를 입력해 주세요 (0을 제외한 숫자): ");
+                    number2 = scanner.nextDouble();
 
-                if (number2 != 0) {
-                    break;
+                    if (number2 != 0) {
+                        break;
+                    }
+                    System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요.");
                 }
-                System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요.");
+            } else {
+                System.out.print("두번째 수를 입력해 주세요: ");
+                number2 = scanner.nextDouble();
             }
-        } else {
-            System.out.print("두번째 수를 입력해 주세요: ");
-            number2 = scanner.nextDouble();
+            switch (operator) {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    result = number1 / number2;
+                    break;
+                default:
+                    System.out.println("잘못된 연산자 입니다.");
+                    ok = false;
+                    break;
+            }
+            if(ok) {
+                System.out.printf("계산된 결과는 %.2f 입니다. \n", result);
+                number1=result;
+            }
+            System.out.println("계산을 계속 하시겠습니까? (Y/N)");
+            String answer = scanner.next();
+
+            if(answer.equals("Y")||answer.equals("y")) {
+            }else{ keep=false; }
+
+            System.out.println("----------------------------------");
         }
-        switch (operator) {
-            case '+':
-                result = number1 + number2;
-                break;
-            case '-':
-                result = number1 - number2;
-                break;
-            case '*':
-                result = number1 * number2;
-                break;
-            case '/':
-                result = number1 / number2;
-                break;
-            default:
-                System.out.println("잘못된 연산자 입니다.");
-                ok = false;
-                break;
-        }
-        if(ok) {
-            System.out.printf("계산된 결과는 %.2f 입니다. ", result);
-        }
+        System.out.printf("최종으로 계산된 결과는 %.2f 입니다.", result);
     }
 }
